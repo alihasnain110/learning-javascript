112ecd307fd7876bf3ec4eeaf2a3f6505c6032f4
alihasnain110 <alihasnain.0335@gmail.com>
2024-09-12 00:50:47 +0500

    javascript assign 1

---
+++ app.js
@@ -0,0 +1,183 @@
+//   CHP6 
+
+
+// Q1
+var x = x + 1;
+ x++ ;
+document.write(x );
+
+// Q2
+
+var x = 100;
+var x = --x;
+document.write("<br>",x);
+
+// Q3 
+
+var x = 50;
+var y = x++;
+document.write("<br>",y);                               
+
+// Q4
+
+var  y = 50 ;
+var z = --y;
+ document.write("<br>",z);
+
+
+//  Q5 
+
+var num = 10;
+var newNum = --num;
+ document.write("<br>", newNum);
+
+//  Q6 
+var m = 50;
+var n = ++m;
+document.write("<br> ",n); 
+
+// Q7 
+
+
+var m = 20;
+var n = ++m;
+alert(n); 
+
+
+//  CHP7
+
+
+// Q1
+
+
+var calculatedNum = 2+(2*6);
+document.write("<br><br> calculatedNum:",calculatedNum);
+
+
+// Q2
+
+var calculatedNum = (2+2)*6;
+document.write("<br> calculatedNum:",calculatedNum);
+
+
+// Q3 
+
+
+var calculatedNum = (2+2)*(4+2);
+document.write("<br> calculatedNum:",calculatedNum);
+
+
+// Q4 
+
+var calculatedNum = ((2 + 2) * 4) + 2;
+document.write("<br> calculatedNum:",calculatedNum);
+
+
+// Q5 
+
+var cost = (2 + 2) * (4 + 10);
+document.write("<br> cost:",cost);
+
+// Q6 
+var units = 2 + 2 * 4 + 10;
+document.write("<br> units:",units);
+
+
+// Q7 
+
+var pressure = 4 / 2 * 4;
+document.write("<br> pressure: ",pressure);
+
+
+
+// CHP 8 
+
+
+
+// Q1 
+
+var num = "2"+"2"
+document.write("<br><br>num :",num);
+
+// Q2 
+
+var message = "Hello "+"world"
+alert(message);
+
+
+
+// Q3
+
+
+
+alert("33"+3);
+ 
+// Q4 
+ 
+
+alert("Pakistan"+"Zindabad");
+
+// Q5 
+
+
+var age = 20;
+document.write("<br> age"+age);
+
+
+// Q6 
+
+var string1 = "Hello" ;
+var string2 = "World";
+
+var result = string1 + string2;
+
+document.write("<br> result:",result);
+
+
+// CHP 9 
+
+
+
+// Q1
+
+
+var firstName = prompt("Enter First name")
+document.write("<br><br> first name :",firstName);
+
+
+// Q2 
+
+var country = prompt ("country"+ "China");
+document.write("<br>country:",country);
+
+
+
+// Q3
+
+// var yourName = prompt(Enter Your Name");
+
+var yourName = prompt("enter your Name");
+
+document.write("<br>yourName:",yourName);
+
+
+// Q4 
+
+
+
+var userinput= prompt("please enter your favourite color ","blue");
+document.write("<br> userinput ", userinput);
+
+
+
+// Q5 
+
+var message =prompt("enter your favourite Friute");
+var Defaultvalue = "Apple";
+ document.write("<br> userresponse:",message,Defaultvalue);
+
+
+//  Q6 
+
+var userresponse = prompt("whats your favrouite movie","inception");
+document.write("you entered :", userresponse);
